e7ffa526052028de2e80bafda455137441d225b3
Emil Grunt <emilgrunt@gmail.com>
2016-05-09 21:15:50 +0200

    Adds opcodes for setting and getting locals and globals

--- guttural/include/gut_opcodes.h
+++ guttural/include/gut_opcodes.h
@@ -4,6 +4,10 @@
 
 enum GutturalOpCode
 {
+    OP_SET_LOCAL,
+    OP_GET_LOCAL,
+    OP_SET_GLOBAL,
+    OP_GET_GLOBAL,
     OP_JUMP,
     OP_ADD,
     OP_SUB,
--- guttural/src/gut_opcodes.c
+++ guttural/src/gut_opcodes.c
@@ -2,6 +2,10 @@
 
 
 char const * const guttural_opcode[] = {
+    "OP_SET_LOCAL",
+    "OP_GET_LOCAL",
+    "OP_SET_GLOBAL",
+    "OP_GET_GLOBAL",
     "OP_JUMP",
     "OP_ADD",
     "OP_SUB",
