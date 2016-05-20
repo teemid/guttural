a94481a7d6265626b203bca49d1832552a6f5c29
Emil Grunt <emilgrunt@gmail.com>
2016-05-20 21:07:53 +0200

    Adds first pass compiler

---
+++ guttural/src/gut_compiler.c
@@ -0,0 +1,16 @@
+#include "gut_compiler.h"
+
+
+#define Function(state) (state)->function
+
+
+void GutCompilerEmit (GutState * state)
+{
+    *state;
+}
+
+
+void GutCompilerAddConstant (GutState * state)
+{
+    *state;
+}
