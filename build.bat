15507092d6daec8ee7efeb382a940f608c5838d7
Emil Grunt <emil.grunt@lumenia.no>
2016-10-06 02:18:29 +0200

    Cleans up build script file

--- build.bat
+++ build.bat
@@ -1,38 +1,15 @@
 @ECHO off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET PROJECT_DIR="%~dp0"
-SET SOURCE_FILES="%~dp0guttural\src\*.c"
-SET INCLUDE_DIR="%~dp0guttural\include\"
-
-
 IF NOT EXIST build mkdir build
 
+IF "%1" == "repl" GOTO repl
 
-IF "%1" == "standalone" GOTO standalone
-IF "%1" == "test" GOTO vmtest
-IF "%1" == "msvc" GOTO msvc
-IF "%1" == "clang" GOTO clang
-
-REM Clang:
-REM -O0: No optimizations
-REM -c: Run preprocessor, parser, LLVM generation, assembler
-REM -g: Generate debug information
-REM -o: Write output to file
-REM -D: Adds an implicit #define into the predefines buffer which is read before the source file is preprocessed.
-REM -I: Adds a specified directory to the search path for include files.
-REM -std: Specify the language stanard to compile for.
-REM -ansi: Alias for -std=c89
-
-
-:standalone
-    SET STANDALONE_FILE = %~dp0guttural\guttural.c
+:repl
     SET MACROS=/DGUTTURAL_DEBUG
-    SET COMPILER_FLAGS=/nologo /Od /W4 /wd4127 /wd4996 /Zi /Fobuild\ /Fdbuild\ /c /I "guttural\include"
+    SET COMPILER_FLAGS=/nologo /Od /W4 /wd4127 /wd4996 /Zi /Fobuild\ /Fobuild\ /c /I "include" /EHsc
 
-    FOR /r %%f IN ("guttural\src\*.c") DO cl %COMPILER_FLAGS% "%%f" %MACROS%
-
-    cl %COMPILER_FLAGS% guttural\guttural.c %MACROS%
+    FOR /r %%f  IN ("source\*.cpp") DO cl %COMPILER_FLAGS% "%%f" %MACROS%
 
     PUSHD build
 
@@ -43,37 +20,6 @@ REM -ansi: Alias for -std=c89
     POPD
 
     GOTO end
-
-:vmtest
-    SET STANDALONE_FILE = %~dp0guttural\guttural.c
-    SET MACROS=/DGUTTURAL_DEBUG
-    SET COMPILER_FLAGS=/nologo /Od /W4 /wd4127 /wd4996 /Zi /Fobuild\ /Fdbuild\ /c /I "guttural\include"
-
-    FOR /r %%f IN ("guttural\src\*.c") DO cl %COMPILER_FLAGS% "%%f" %MACROS%
-
-    cl %COMPILER_FLAGS% guttural\test.c %MACROS%
-
-    PUSHD build
-
-    SET LINKER_FLAGS=/NOLOGO /DEBUG
-
-    link %LINKER_FLAGS% /OUT:test_guttural.exe *.obj
-
-    POPD
-
-    GOTO end
-
-:msvc
-    SET COMPILER=cl
-    SET CL_FLAGS=/nologo /I "guttural\include" /Fobuild\
-    SET CL_MACROS=/DGUTTURAL_DEBUG
-
-    FOR /r %%f IN (guttural\src\*.c) DO %COMPILER% %CL_FLAGS% "%%f" %CL_MACROS%
-
-    GOTO end
-
-:clang
-    SET CLANG_FLAGS=-O0 -g -c -I "guttural\include"
 
 :end
     ECHO "Finished"
